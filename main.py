548b13ef345527f4a5745e7cac35f1c64575a804
brown-lyssie <abrown@nevada.unr.edu>
2023-05-05 14:54:05 -0700

    (muffled screaming) datagrams, sim created

--- main.py
+++ main.py
@@ -2,15 +2,12 @@ import sys
 from parse_cla import parse_cla
 from classes.Network import Network
 from classes.Packet import Packet
+from Simulator import Simulator
 def main(clean_argc, clean_argv):
     print(clean_argc)
     print(clean_argv)
     with open("LOG.txt", "a") as f:
-        network = Network(chance_of_host_failure=.02, log=True, log_file_obj = f)
-        network.add_host_to_network("1:1:1:1:1:1:1:1")
-        network.add_host_to_network("2:2:2:2:2:2:2:2")
-        network.insert_incoming_packet("1:1:1:1:1:1:1:1", Packet("hello world"), log=True, log_file_obj=f)
-        network.transfer_messages_from_hosts("1:1:1:1:1:1:1:1", "2:2:2:2:2:2:2:2", log=True, immediate=True)
+        sim = Simulator()
 
 
 if __name__ == "__main__":
